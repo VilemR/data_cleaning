6943ffd4aefe3346982005f39b8dfc21b9252859
VilemR <vilem.reznicek@email.cz>
2018-12-22 20:10:21 +0100

    commit

---
+++ KniterRun.R
@@ -0,0 +1,13 @@
+install.packages("knitr")
+
+install.packages("markdown")
+
+require(knitr)
+require(markdown)
+
+setwd('/home/vilem_reznicek/Development/RStudio_projects/RStudio_projects/coursera')
+knit("hpaData.R", encoding="ISO8859-1")
+markdownToHTML("hpaData.R", "hpaData.R.html")
+
+
+#https://github.com/benjamin-chan/GettingAndCleaningData
